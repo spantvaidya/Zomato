a29bfe3b6cbaa25878101397a9b0644c5073ecdc
Sameer Pantvaidya <spantvaidya@deloitte.com>
2025-03-06 16:22:35 +0530

    ZomatoEmailAPI_InitialSetup

---
+++ Zomato.Services.EmailAPI/Models/Dto/ProductDto.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace Zomato.Services.EmailAPI.Models.Dto
+{
+    public class ProductDto
+    {
+        public int ProductId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(0.01, 10000.00)]
+        public double Price { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Url]
+        [ValidateNever]
+        public string? ImageUrl { get; set; }
+        [Range(1, 100)]
+        public int Count { get; set; } = 1;
+    }
+}
---
+++ Zomato.Services.EmailAPI/Program.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using Zomato.Services.EmailAPI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(option =>
+option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+//Add Pending Migration at the start of the App
+ApplyMigration();
+
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbInitilizer = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        if (dbInitilizer.Database.GetPendingMigrations().Count() > 0)
+        {
+            dbInitilizer.Database.Migrate();
+        };
+    }
+}
+
+app.Run();
